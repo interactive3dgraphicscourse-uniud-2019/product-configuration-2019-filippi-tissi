6beaec9c7004789a73e2ef18afcfdbfdaa465634
Simone Tissi <simone.tissi@gmail.com>
2019-05-29 11:32:02 +0200

    Edited texture

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,7 @@ function loadTexture(file) {
     var texture = new THREE.TextureLoader().load( file , function ( texture ) {
 
         texture.minFilter = THREE.LinearMipMapLinearFilter;
-        texture.anisotropy = renderer.getMaxAnisotropy();
+        texture.anisotropy = renderer.capabilities.getMaxAnisotropy();
         texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
         texture.offset.set( 0, 0 );
         texture.needsUpdate = true;
@@ -90,17 +90,18 @@ function init(){
 
 					] );
 
-			scene.background = textureCube;    //se lo si decommenta aggiungo il background alla scena ma a noi non serve
+			//scene.background = textureCube;    //se lo si decommenta aggiungo il background alla scena ma a noi non serve
             textureCube.minFilter = THREE.LinearMipMapLinearFilter;
             
     // ----------------------------------------------------------------------------      
 
 
     var uniforms_cloth={
+        specularMap:    { type: "t", value: loadTexture("textures/cloth/Carpet_Specular.jpg") },
         diffuseMap:		{ type: "t", value: loadTexture("textures/cloth/Carpet_Diffuse.jpg") },
         roughnessMap:	{ type: "t", value: loadTexture("textures/cloth/Carpet_Roughness.jpg") },
         normalMap:		{ type: "t", value: loadTexture("textures/cloth/Carpet_Normal.jpg") },
-        normalScale:	{ type: "v2", value: new THREE.Vector2(0.5, 0.5) },     //valori da modificare
+        aoMap:	    	{ type: "t", value: loadTexture("textures/cloth/Carpet_AO.jpg") },
         textureRepeat:	{ type: "v2", value: new THREE.Vector2(4.0, 4.0) }      //valori da modificare
     }
 
@@ -108,91 +109,67 @@ function init(){
         surfCdiff: { type: "v3", value: new THREE.Vector3(1.0, 1.0, 1.0) }
     }
 
-    var lightParameters = {
-        red: 1.0,
-        green: 1.0,
-        blue: 1.0,
-        intensity: 1.0,
-        pos: [0,10,0]
-    };
+    var param_luce1 = { red: 1.0, green: 1.0, blue: 1.0,    intensity: 0.5,    pos: [0, 10, 0] };
+    var param_luce2 = { red: .2, green: .2, blue: .2,    intensity: 0.4,    pos: [10, -10, 0] };
 
-    var param_luce1 = { red: 1.0, green: 1.0, blue: 1.0,    intensity: 0.5,    pos: [0, 40, 0] };
-    var param_luce2 = { red: .2, green: .2, blue: .2,    intensity: 0.4,    pos: [20, -30, 0] };
+    var ambientLightParams = { red: 0.1, green: 0.05, blue: 0.05, intensity: 0.1, }
 
     if (param_luce1.intensity > 0) {
         var sole1 = new THREE.Mesh( new THREE.SphereGeometry( 1, 16, 16), new THREE.MeshBasicMaterial ({color: 0xffff00, wireframe:true}));
         sole1.position.set( param_luce1.pos[0], param_luce1.pos[1], param_luce1.pos[2] );
         //scene.add(sole1);
         var raggio1 = new THREE.Vector3(sole1.position.x, sole1.position.y, sole1.position.z);
-    } else { var lightPos1 = new THREE.Vector3(0,0,0); }
+    } else { var raggio1 = new THREE.Vector3(0,0,0); }
     
     if (param_luce2.intensity > 0) {
         var sole2 = new THREE.Mesh( new THREE.SphereGeometry( 1, 16, 16), new THREE.MeshBasicMaterial ({color: 0xffff00, wireframe:true}));
         sole2.position.set( param_luce2.pos[0], param_luce2.pos[1], param_luce2.pos[2] );
         //scene.add(sole2);
         var raggio2 = new THREE.Vector3(sole2.position.x, sole2.position.y, sole2.position.z)
+    }else { var raggio2 = new THREE.Vector3(0,0,0); }
+
+
+    var unif_condivisi = {
+        pointLightPositions: {
+            type: "v3[]",
+            value: new Array(raggio1, raggio2)
+        },
+        clights: {
+            type: "v3[]",
+            value: new Array(
+                new THREE.Vector3(
+                    param_luce1.red * param_luce1.intensity,
+                    param_luce1.green * param_luce1.intensity,
+                    param_luce1.blue * param_luce1.intensity),
+                new THREE.Vector3(
+                    param_luce2.red * param_luce2.intensity,
+                    param_luce2.green * param_luce2.intensity,
+                    param_luce2.blue * param_luce2.intensity)
+            )
+        },
+        ambientLight: {
+            type: "v3",
+            value: new THREE.Vector3(
+                ambientLightParams.red * ambientLightParams.intensity,
+                ambientLightParams.green * ambientLightParams.intensity,
+                ambientLightParams.blue * ambientLightParams.intensity)
+        }
     }
 
-
-
-        var unif_condivisi = {
-            pointLightPositions: {
-                type: "v3[]",
-                value: new Array(param_luce1, param_luce2)
-            },
-            clights: {
-                type: "v3[]",
-                value: new Array(
-                    new THREE.Vector3(
-                        param_luce1.red * param_luce1.intensity,
-                        param_luce1.green * param_luce1.intensity,
-                        param_luce1.blue * param_luce1.intensity),
-                    new THREE.Vector3(
-                        param_luce2.red * param_luce2.intensity,
-                        param_luce2.green * param_luce2.intensity,
-                        param_luce2.blue * param_luce2.intensity)
-                )
-            },
-            ambientLight: {
-                type: "v3",
-                value: new THREE.Vector3(
-                    ambientLightParams.red * ambientLightParams.intensity,
-                    ambientLightParams.green * ambientLightParams.intensity,
-                    ambientLightParams.blue * ambientLightParams.intensity)
-            },
-             envMap:	{ type: "t", value: textureCube} 
-        }
-
-
-    
-    var lightPos1 = new THREE.Vector3(10,10,10);
+    Object.assign(sitting_uniforms, unif_condivisi);
     Object.assign(sitting_uniforms, uniforms_cloth);
 
 
     vs = document.getElementById("vertex").textContent;
     fs = document.getElementById("fragment").textContent;
     var sitting_material=new THREE.ShaderMaterial({uniforms: sitting_uniforms, vertexShader: vs, fragmentShader: fs});
+    sitting.needsUpdate = true;
     sitting.forEach(function(el){
         el.material = sitting_material;
     });
-    //Coordinates.drawAllAxes(); //disegna gli assi
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    requestAnimationFrame(update);
+    
+    Coordinates.drawAllAxes(); //disegna gli assi
 
 
 
